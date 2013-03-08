5c7bd41888ff084deb7ed3af4ecc9e5d8e19f0e8
Luke Boening <luke@boening.us>
2013-03-08 17:25:20 -0600

    Synch with agile zen card #30
    This is supposed to synch and update the card. Does it?

--- install_python.cmd
+++ install_python.cmd
@@ -8,3 +8,4 @@ powershell -noexit & "c:\alldata\install_python.ps1"
 pause
 :: Practice pull request aimed at Agilezen card #30
 :: Another practice line: did not work last time. Target: #30
+:: Attempt number two to get this commit to synchronize with agile zen card #30
