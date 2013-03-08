afba709baa85462f46f39a683361846ea72de32e
Luke Boening <luke@boening.us>
2013-03-08 13:06:55 -0600

    Initial commit of install_python.cmd file.

---
+++ install_python.cmd
@@ -0,0 +1,8 @@
+@ECHO OFF
+:: install_python.cmd
+:: Record content author: Luke Boening
+:: Revision: 0.1
+:: Git version: %here%
+:: Last update: 2013-03-13
+powershell -noexit & "c:\alldata\install_python.ps1"
+pause
