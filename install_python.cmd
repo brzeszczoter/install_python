04512e408a98b1e813f82b95f4fc78274ba86c62
Luke Boening <luke@boening.us>
2013-03-11 07:43:54 -0500

    Changed file name to get-python2.ps1 #30
    Changed file name to get-python2.ps1

--- install_python.cmd
+++ install_python.cmd
@@ -1,10 +1,10 @@
 @ECHO OFF
 :: install_python.cmd
 :: Record content author: Luke Boening
-:: Revision: 0.1
+:: Revision: 0.11
 :: Git version: %here%
-:: Last update: 2013-03-13
-powershell -noexit & "c:\alldata\install_python.ps1"
+:: Last update: 2013-03-11
+powershell -noexit & "c:\alldata\get-python2.ps1"
 pause
 :: Practice pull request aimed at Agilezen card #30
 :: Another practice line: did not work last time. Target: #30
