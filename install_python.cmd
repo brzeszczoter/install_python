71f6b07d76e0feeff7b67edaa01fb19a3c8ff47f
Luke Boening <luke@boening.us>
2013-04-19 14:08:58 -0500

    Commit synch with AgileZen card number thirty-one #31
    Testing: synch changeset on Agilezen card #31

--- install_python.cmd
+++ install_python.cmd
@@ -1,10 +1,11 @@
 @ECHO OFF
 :: install_python.cmd
 :: Record content author: Luke Boening
-:: Revision: 0.12
+:: Revision: 0.12.1
 :: Git version: %here%
-:: Last update: 2013-04-17
+:: Last update: 2013-04-19
 powershell -noexit & "c:\alldata\get-python2.ps1"
 pause
 :: Adding a commit to agile zen card #31
+:: Commit synch with AgileZen card number thirty-one #31
 
